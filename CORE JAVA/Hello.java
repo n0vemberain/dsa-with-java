6a68f18c8f4ca33a06578707efedfed007745a37
Fatin Ishraque <155258978+n0vemberain@users.noreply.github.com>
2025-11-13 21:41:23 +0600

    New lesson started

---
+++ CORE JAVA/Hello.java
@@ -0,0 +1,9 @@
+class Hello {
+    public static void main (String args[]){
+        System.out.println("Hello, World!");
+        int x = 0b101 ;
+        int y = 10 ;
+        int sum = x + y ;
+        System.out.println("Total : " + sum);
+    }
+}
